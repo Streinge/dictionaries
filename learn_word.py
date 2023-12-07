38d863efb85c17e414f05dab809ff83020158a1c
Streinge <olesof@mail.ru>
2023-12-07 11:51:22 +0500

    рабочий 7.12 11:51

--- learn_word.py
+++ learn_word.py
@@ -2,8 +2,8 @@
 import codecs
 import random
 NUMBER_REPIT_WORDS = 50
-WORD_FILE_NAME = 'dict.txt'
-# WORD_FILE_NAME = 'new_word.txt'
+# WORD_FILE_NAME = 'dict.txt'
+WORD_FILE_NAME = 'new_word.txt'
 # WORD_FILE_NAME = 'repeat_word.txt'
 status = False
 dict = {}
